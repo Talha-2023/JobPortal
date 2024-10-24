165635cf3567c2d464fdb8e9f9d12c6b8458a7d5
talha_2024 <syedtalhaahmed123786@gmail.com>
2024-10-24 16:15:49 +0530

    created custome error message

--- backend/app.js
+++ backend/app.js
@@ -5,14 +5,34 @@ const morgan = require("morgan");
 const bodyParser = require("body-parser");
 require("dotenv").config();
 var cors = require("cors");
+const cookieParser = require("cookie-parser");
+const errorHandler = require("./middleware/error");
 
-//DB CONNECT
+//*---Db Connection
 mongoose
   .connect(process.env.DATABASE)
   .then(() => console.log("DB CONNECTED"))
   .catch((err) => console.log(err));
 
-//PORT
+//*---Middleware
+
+app.use(morgan("dev"));
+app.use(bodyParser.json({ limit: "5mb" }));
+app.use(
+  bodyParser.urlencoded({
+    limit: "5mb",
+    extended: true,
+  })
+);
+
+app.use(cookieParser());
+app.use(cors());
+
+//*---error middleware
+
+app.use(errorHandler);
+
+//*---Port
 const port = process.env.PORT || 8001;
 
 app.get("/", (req, res) => {
